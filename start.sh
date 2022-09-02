b60385551babb4db0538586c2f1b03ad535c502d
Himanshu Rastogi <98731354+HimanshuRastogiOfficial@users.noreply.github.com>
2022-09-03 01:22:57 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,6 @@
-if [ -z $UPSTREAM_REPO ]
-then
-  echo "Cloning main Repository"
-  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
-else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
-fi
-cd /EvaMaria
+echo "Cloning main Repository"
+git clone https://github.com/Petriona/TheTheta /TheTheta
+cd /TheTheta
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
