60b56df106e0d075f726ecf03f9d8b9c96e887e9
Himanshu Rastogi <98731354+HimanshuRastogiOfficial@users.noreply.github.com>
2022-09-03 01:00:19 +0530

    Update genlink.py

--- plugins/genlink.py
+++ plugins/genlink.py
@@ -1,14 +1,14 @@
 import re
-from pyrogram import filters, Client, enums
-from pyrogram.errors.exceptions.bad_request_400 import ChannelInvalid, UsernameInvalid, UsernameNotModified
-from info import ADMINS, LOG_CHANNEL, FILE_STORE_CHANNEL, PUBLIC_FILE_STORE
-from database.ia_filterdb import unpack_new_file_id
-from utils import temp
-import re
 import os
 import json
 import base64
 import logging
+from info import ADMINS
+from pyrogram import filters, Client
+from helper import unpack_new_file_id
+from pyrogram.errors.exceptions.bad_request_400 import ChannelInvalid
+from pyrogram.errors.exceptions.bad_request_400 import UsernameInvalid
+from pyrogram.errors.exceptions.bad_request_400 import UsernameNotModified
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -20,24 +20,7 @@ async def allowed(_, __, message):
         return True
     return False
 
-@Client.on_message(filters.command(['link', 'plink']) & filters.create(allowed))
-async def gen_link_s(bot, message):
-    replied = message.reply_to_message
-    if not replied:
-        return await message.reply('Reply to a message to get a shareable link.')
-    file_type = replied.media
-    if file_type not in [enums.MessageMediaType.VIDEO, enums.MessageMediaType.AUDIO, enums.MessageMediaType.DOCUMENT]:
-        return await message.reply("Reply to a supported media")
-    if message.has_protected_content and message.chat.id not in ADMINS:
-        return await message.reply("okDa")
-    file_id, ref = unpack_new_file_id((getattr(replied, file_type.value)).file_id)
-    string = 'filep_' if message.text.lower().strip() == "/plink" else 'file_'
-    string += file_id
-    outstr = base64.urlsafe_b64encode(string.encode("ascii")).decode().strip("=")
-    await message.reply(f"Here is your Link:\nhttps://t.me/{temp.U_NAME}?start={outstr}")
-    
-    
-@Client.on_message(filters.command(['batch', 'pbatch']) & filters.create(allowed))
+@Client.on_message(filters.command('index') & filters.create(allowed))
 async def gen_link_batch(bot, message):
     if " " not in message.text:
         return await message.reply("Use correct format.\nExample <code>/batch https://t.me/TeamEvamaria/10 https://t.me/TeamEvamaria/20</code>.")
@@ -105,7 +88,6 @@ async def gen_link_batch(bot, message):
                     "caption": caption,
                     "title": getattr(file, "file_name", ""),
                     "size": file.file_size,
-                    "protect": cmd.lower().strip() == "/pbatch",
                 }
 
                 og_msg +=1
@@ -122,4 +104,4 @@ async def gen_link_batch(bot, message):
     post = await message.reply_document(f"batchmode_{message.from_user.id}.json", file_name="Batch.json", caption="⚠️Generated for filestore.")
     os.remove(f"batchmode_{message.from_user.id}.json")
     file_id, ref = unpack_new_file_id(post.document.file_id)
-    await sts.edit(f"Here is your link\nContains `{og_msg}` files.\n https://t.me/{temp.U_NAME}?start=BATCH-{file_id}")
+    await sts.edit(f"Here is your link\nContains `{og_msg}` files.\n https://t.me/instagramkarobot?start=BATCH-{file_id}")
